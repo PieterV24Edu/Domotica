763a1f53fc26f900e913bbc9542b070d804585ac
Pieter Verweij <pieter1998.verweij@gmail.com>
2015-12-17 11:34:33 +0100

    Added: * Code in mainprogram * Check for socket input

--- DomoticaArduino/DomoticaArduino.ino
+++ DomoticaArduino/DomoticaArduino.ino
@@ -46,14 +46,37 @@ void loop() {
 
     if(count > 0)
     {
-      
+      //Channel 1
+      if(String(buffer) == String("Ch1ON")) States[0] = true;
+      else if(String(buffer) == String("Ch1OFF")) States[0] = false;
+      //Channel 2
+      if(String(buffer) == String("Ch2ON")) States[1] = true;
+      else if(String(buffer) == String("Ch2OFF")) States[1] = false;
+      //Channel 3
+      if(String(buffer) == String("Ch3ON")) States[2] = true;
+      else if(String(buffer) == String("Ch3OFF")) States[2] = false;
+      //Channel 4
+      if(String(buffer) == String("Ch4ON")) States[3] = true;
+      else if(String(buffer) == String("Ch4OFF")) States[3] = false;
+      //Channel All
+      if(String(buffer) == String("ChAllON")) States[4] = true;
+      else if(String(buffer) == String("ChAllOFF")) States[4] = false;
+      //Return stuff
+      //if(String(buffer) == String("getVal")) returnValues();
+      //
+      mainProgram();
     }
   }
 }
 
 void mainProgram()
 {
-  
+  for(int i = 0; i < 5; i++)
+  {
+    if(States[i] = true) mySwitch.send(Freq[i][1], 24);
+    else mySwitch.send(Freq[i][0], 24);
+    delay(1000);
+  }
 }
 
 void returnValues()
@@ -63,9 +86,9 @@ void returnValues()
 
 void resetAll()
 {
-  mySwitch.send(Freq[5][0], 24);
+  mySwitch.send(Freq[4][0], 24);
   delay(5000);
-  mySwitch.send(Freq[5][0], 24);
+  mySwitch.send(Freq[4][0], 24);
   delay(5000);
 }
 
