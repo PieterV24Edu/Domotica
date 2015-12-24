a5a1ba31b9502d65bcade373f71d948c7455304f
Pieter <pieter1998.verweij@gmail.com>
2015-12-24 13:25:52 +0100

    Added: *New Solution with (partially)Working drawer Menu
    ToDo:
    *Port existing app to the new app with the menu

---
+++ Domotica/Domotica/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.V7.App;
+using Android.Support.V4.Widget;
+
+namespace Domotica
+{
+	[Activity (Label = "Domotica", MainLauncher = true, Icon = "@mipmap/icon", Theme="@style/MyTheme")]
+	public class MainActivity : AppCompatActivity
+	{
+		private SupportToolbar mToolbar;
+		private MyActionBarDrawerToggle mDrawerToggle;
+		private DrawerLayout mDrawerLayout;
+		private ListView mDrawer;
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			mToolbar = FindViewById<SupportToolbar> (Resource.Id.toolbar);
+			mDrawerLayout = FindViewById<DrawerLayout> (Resource.Id.drawer_layout);
+			mDrawer = FindViewById<ListView> (Resource.Id.left_drawer);
+
+			SetSupportActionBar (mToolbar);
+			SupportActionBar.SetHomeButtonEnabled(true);
+			SupportActionBar.SetDisplayHomeAsUpEnabled(true);﻿
+
+			mDrawerToggle = new MyActionBarDrawerToggle (
+				this,
+				mDrawerLayout,
+				Resource.String.openDrawer,
+				Resource.String.closeDrawer
+			);
+
+			mDrawerLayout.SetDrawerListener (mDrawerToggle);
+			SupportActionBar.SetHomeButtonEnabled (true);
+			SupportActionBar.SetDisplayShowTitleEnabled (true);
+			mDrawerToggle.SyncState ();
+		}
+
+		public override bool OnOptionsItemSelected (IMenuItem item)
+		{
+			mDrawerToggle.OnOptionsItemSelected (item);
+			return base.OnOptionsItemSelected (item);
+		}
+	}
+}
+
+
